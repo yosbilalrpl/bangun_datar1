ba998f224613320f1c3acce913b3a6d22ffc4dde
yosbilalrpl <yosbilai7@gmail.com>
2023-11-14 13:18:06 +0700

    first commit

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -7,7 +7,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          backgroundColor: Colors.red.shade300,
+          backgroundColor: Colors.cyan.shade300,
           title: Text(
               "Home Page",
               style: TextStyle(
@@ -17,27 +17,60 @@ class HomePage extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Container(
-      padding: EdgeInsets.symmetric(vertical:8, horizontal:16),
-      decoration: BoxDecoration(
-          color: Colors.green
-
-      ),
-      child: Text("Persegi"),
-
-    ),
+          customMenu(ImageAssets: "assets/persegiblue.png",title: "persegi"),
+          customMenu(ImageAssets: "assets/segitiga.png",title: "segitiga"),
       Container(
         margin: EdgeInsets.all(8),
         padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
         decoration: BoxDecoration(
           color: Colors.green
       ),
-        child: Text("segitiga"),
+        child: Column(
+          children: [
+            Image.asset("assets/segitiga.png", height: 100,),
+            Text("segitiga"),
+          ],
+        ),
 
           )
         ],
 
       ),
     );
+  }
+}
+
+class customMenu extends StatelessWidget {
+  const customMenu({
+    super.key, required this.ImageAssets, required this.title,
+  });
+ final String ImageAssets;
+ final String title;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(8),
+      padding: EdgeInsets.symmetric(vertical:8, horizontal:16),
+      decoration: BoxDecoration(
+    color: Colors.green
+
+      ),
+      child: Column(
+        children: [
+    Image.asset(ImageAssets, height: 100,),
+    Text(title),
+        ],
+      ),
+
+    );
+  }
+}
+
+class  extends StatelessWidget {
+  const ({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
---
+++ lib/pages/new_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class new_page extends StatelessWidget {
+  const new_page({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
