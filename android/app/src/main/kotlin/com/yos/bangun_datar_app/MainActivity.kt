1ec8fe826951911d69adc8e25698cd97e8fc5f84
yosbilalrpl <yosbilai7@gmail.com>
2023-11-14 11:58:19 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/yos/bangun_datar_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.yos.bangun_datar_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          backgroundColor: Colors.red.shade300,
+          title: Text(
+              "Home Page",
+              style: TextStyle(
+                color: Colors.white,
+              )
+          )
+      ),
+      body: Column(
+        children: [
+          Container(
+      padding: EdgeInsets.symmetric(vertical:8, horizontal:16),
+      decoration: BoxDecoration(
+          color: Colors.green
+
+      ),
+      child: Text("Persegi"),
+
+    ),
+      Container(
+        margin: EdgeInsets.all(8),
+        padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+        decoration: BoxDecoration(
+          color: Colors.green
+      ),
+        child: Text("segitiga"),
+
+          )
+        ],
+
+      ),
+    );
+  }
+}
